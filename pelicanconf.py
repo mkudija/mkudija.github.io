508226def056688918b713dc7d3fca8fdc1bb405
Matthew Kudija <m.kudija@gmail.com>
2017-06-06 06:36:49 -0400

    re-export

--- pelicanconf.py
+++ pelicanconf.py
@@ -34,4 +34,4 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-#THEME = 'pelican-themes/subtle'
+THEME = 'pelican-themes/subtle'
