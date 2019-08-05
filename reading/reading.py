68c7a182d10575e093f893c1c6b2f541feb4d4ec
Matthew Kudija <m.kudija@gmail.com>
2019-08-04 21:22:03 -0400

    break between audio and book

--- reading/reading.py
+++ reading/reading.py
@@ -17,32 +17,62 @@ def write_html(df, year):
         html_file.write("%s\n" % book)
     html_file.close()
 
+    numReadAll = df.shape[0]
+    numRead = df[df['Type']!='Audiobook'].shape[0]
+
     try:
         npages  = df.loc[:,'Pages'].sum()
-        print('In {} I read {} books ({}k pages).'.format(str(year), str(df.shape[0]), str(int(npages/1000))))
+        print('In {} I read {} books ({} read, {}k pages).'.format(str(year), 
+                                                                   str(numReadAll), 
+                                                                   str(numRead),
+                                                                   str(int(npages/1000))
+                                                                   )
+              )
     except:
-        print('In {} I read {} books.'.format(str(year), str(df.shape[0])))
+        print('In {} I read {} books ({} read).'.format(str(year), 
+                                                        str(numReadAll),
+                                                        str(numRead)
+                                                        ) 
+             )
 
-    # forecast reading
-    bookForecast = 0
+    # forecast reading (including audiobooks)
+    bookForecastAll = 0
     from datetime import datetime
     if year==datetime.now().year:
         booksToDate = df.dropna(how='any',axis=0).shape[0]
         pagesToDate = df.dropna(how='any',axis=0).loc[:,'Pages'].sum()
         dayOfYear = datetime.now().timetuple().tm_yday
-        bookForecast = booksToDate / dayOfYear * 365
-        pageForecast = pagesToDate / dayOfYear * 365
-        print('...on track to read {} books ({}k pages) in {}'.format(int(bookForecast), int(pageForecast/1000), year))
-    return bookForecast
- 
+        bookForecastAll = booksToDate / dayOfYear * 365
+        pageForecastAll = pagesToDate / dayOfYear * 365
 
-def plot_books(df, years, bookForecast):
+    # forecast reading (less audiobooks)
+    df = df[df['Type']!='Audiobook']
+    bookForecastRead = 0
+    from datetime import datetime
+    if year==datetime.now().year:
+        booksToDate = df.dropna(how='any',axis=0).shape[0]
+        pagesToDate = df.dropna(how='any',axis=0).loc[:,'Pages'].sum()
+        dayOfYear = datetime.now().timetuple().tm_yday
+        bookForecastRead = booksToDate / dayOfYear * 365
+        pageForecastRead = pagesToDate / dayOfYear * 365
+        print('...on track to read {} ({}) books ({}k ({}k) pages) in {}'.format(int(bookForecastAll),
+                                                                                 int(bookForecastRead),
+                                                                                 int(pageForecastAll/1000),
+                                                                                 int(pageForecastRead/1000),
+                                                                                 year)
+              )
+
+    return bookForecastAll, bookForecastRead
+
+
+def plot_books(df, years, bookForecastAll, bookForecastRead):
     import matplotlib.pyplot as plt
 
     print('\nPlotting...')
 
     # don't plot 2005 and before
     df = df[df['Read']!=2005]
+    df2 = df[df['Type']!='Audiobook']
     years = years[1:]
 
     number = []
@@ -50,17 +80,22 @@ def plot_books(df, years, bookForecast):
         df_count = df[df['Read']==year]
         number.append(df_count.shape[0])
 
+    numberRead = []
+    for year in years:
+        df_count = df2[df2['Read']==year]
+        numberRead.append(df_count.shape[0])
+
     fig, ax = plt.subplots()
     ax.axis('off')
-    plt.bar(years[-1], bookForecast, color='#ffffff', edgecolor='#909090')
+    plt.bar(years[-1], bookForecastAll, color='#ffffff', edgecolor='#909090')
     plt.bar(years, number, color='#3377b3')
+    plt.bar(years, numberRead, color='#2a6394')
     for item in range(0,len(years)):
-        #plt.text(years[item], number[item]+3, str(number[item]),ha='center',color='#63666a',fontname='Gill Sans MT',fontsize=14)
         plt.text(years[item], -8, str(years[item]),ha='center',color='#63666a',fontname='Gill Sans MT',fontsize=14)
     fig.set_size_inches(12, 4)
-    fig.savefig(str(GitHubPath)+'/mkudija.github.io/images/book_plot.png'
-                , bbox_inches='tight'
-                , transparent=True)
+    fig.savefig(str(GitHubPath)+'/mkudija.github.io/images/book_plot.png',
+                bbox_inches='tight',
+                transparent=True)
 
 
 def copy_file(src, dst):
@@ -127,9 +162,9 @@ if __name__ == "__main__":
     years = df['Read'].unique()
 
     for year in years:
-        bookForecast = write_html(df, year)
+        bookForecastAll, bookForecastRead = write_html(df, year)
     
-    plot_books(df, years, bookForecast)
+    plot_books(df, years, bookForecastAll, bookForecastRead)
 
     print('\nBuilding reading.html...')
     construct_index(years, pathOutput='../reading.html')
